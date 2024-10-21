40953e563480ac7727953823f1101ccf90a231e6
AnilmarChoque <anilmarorellana@gmail.com>
2024-10-21 14:20:50 -0300

    style: estilização das KPIs e do envio de arquivo .csv

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams, useLocation, useNavigate} from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
 import "./Dashboard.css";
 import Perfil from "../../components/Perfil";
@@ -7,7 +7,7 @@ import Back from "../../assets/back.png";
 import fotoPerfil from "../../assets/profile-user.png";
 import SearchBar from "../../components/SearchBar";
 import search from "../../assets/search.png";
-import MiniCard from "../../components/MiniCard"
+import MiniCard from "../../components/MiniCard";
 
 const GET_SENSOR = gql`
 	query GetSensor($idSensor: ID!) {
@@ -33,7 +33,7 @@ const GET_SENSORES = gql`
 
 const Dashboard = () => {
 	const { id } = useParams();
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 	const location = useLocation();
 
 	const { usuarioDados } = location.state;
@@ -42,19 +42,18 @@ const Dashboard = () => {
 		variables: { idSensor: id },
 	});
 
-    const { data: sensoresData } = useQuery(GET_SENSORES, {
+	const { data: sensoresData } = useQuery(GET_SENSORES, {
 		variables: { idEmpresa: usuarioDados.login.idEmpresa },
 		pollInterval: 5000,
 	});
 
-    const [filteredSensors, setFilteredSensors] = useState(null);
+	const [filteredSensors, setFilteredSensors] = useState(null);
 
-    const sensores = sensoresData?.sensoresPorEmpresa || [];
+	const sensores = sensoresData?.sensoresPorEmpresa || [];
 
-    const handleSearch = (searchTerm) => {
+	const handleSearch = (searchTerm) => {
 		if (searchTerm) {
-			// Filtrar sensores com base na pesquisa
-			const filtered = sensores.filter(sensor =>
+			const filtered = sensores.filter((sensor) =>
 				sensor.equipmentId.toLowerCase().includes(searchTerm.toLowerCase())
 			);
 			setFilteredSensors(filtered);
@@ -63,18 +62,14 @@ const Dashboard = () => {
 		}
 	};
 
-    const noResults = filteredSensors !== null && filteredSensors.length === 0;
-	
+	const noResults = filteredSensors !== null && filteredSensors.length === 0;
+
 	const sensorsToDisplay = filteredSensors != null ? filteredSensors : sensores;
 
 	if (loading) return <p>Carregando...</p>;
 	if (error) return <p>Erro ao carregar o sensor</p>;
 
 	const { equipmentId, timestamp, value } = data.getSensor;
-
-    const handleBackPage = () => {
-        navigate(`/home`, {state: { data: usuarioDados}});
-    };
 
 	return (
 		<div className="dashboard">
@@ -93,11 +88,16 @@ const Dashboard = () => {
 					left="0"
 				/>
 				<div className="div-search">
-					<SearchBar backgroundColor="#000524" lupa={search} size="90%" onSearch={handleSearch}/>
+					<SearchBar
+						backgroundColor="#000524"
+						lupa={search}
+						size="90%"
+						onSearch={handleSearch}
+					/>
 				</div>
 
 				<div className="menu-cards">
-                {noResults ? (
+					{noResults ? (
 						<p className="aviso">Nenhum sensor encontrado</p>
 					) : sensorsToDisplay.length === 0 ? (
 						<p className="aviso">Nenhum sensor cadastrado</p>
@@ -108,14 +108,67 @@ const Dashboard = () => {
 								identificador={sensor.equipmentId}
 								temperaturaSensor={sensor.value}
 								idSensor={sensor.idSensor}
-								dados={ usuarioDados }
+								dados={usuarioDados}
 							/>
 						))
 					)}
 				</div>
 			</section>
 			<section className="menu-dashboard">
-				<p>EQUIPAMENTO: {equipmentId}</p>
+				<p className="titulo-dashboard">EQUIPAMENTO: {equipmentId}</p>
+				<div className="grafico-csv">
+					<div className="grafico">
+						<div style={divStyle}>
+							<JSCharting options={config} />
+						</div>
+					</div>
+					<div className="arquivo-csv">
+						<p>Problemas com dados incompletos?</p>
+						<p>Envie um arquivo .CSV</p>
+						<label class="file">
+							Clique aqui para enviar
+							<input type="file" accept=".csv" />
+						</label>
+					</div>
+				</div>
+				<div className="titulo-legenda">
+					<div className="titulo">
+						<p>Média de Temperatura</p>
+					</div>
+					<div className="legenda">
+						<p className="titulo-legenda">Legenda:</p>
+						<div className="container-bom">
+							<div className="bom"></div>
+							<p>Bom</p>
+						</div>
+						<div className="container-neutro">
+							<div className="neutro"></div>
+							<p>Neutro</p>
+						</div>
+						<div className="container-ruim">
+							<div className="ruim"></div>
+							<p>Ruim</p>
+						</div>
+					</div>
+				</div>
+				<div className="medias">
+					<div className="dia">
+						<p className="titulo-media">Últimas 24 horas</p>
+						<p className="temperatura-dia">53.72°C</p>
+					</div>
+					<div className="dias">
+						<p className="titulo-media">Últimas 48 horas</p>
+						<p className="temperatura-dias">78.24°C</p>
+					</div>
+					<div className="semana">
+						<p className="titulo-media">Última Semana</p>
+						<p className="temperatura-semana">103.57°C</p>
+					</div>
+					<div className="mes">
+						<p className="titulo-media">Último Mês</p>
+						<p className="temperatura-mes">84.43°C</p>
+					</div>
+				</div>
 			</section>
 		</div>
 	);
